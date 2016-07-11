1149420d1fd9b9ed217bf93eb8285241e1bb9e89
yazid <flight846@gmail.com>
2016-07-11 20:28:23 +0800

    Add test

--- test/me_test.js
+++ test/me_test.js
@@ -32,6 +32,34 @@ describe('GET /', () => {
   })
 })
 
+// GET social me
+describe('GET /social', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.get('/social')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+  it('should return an array', (done) => {
+    api.get('/social')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body.length).to.equal(response.body.length)
+      done()
+    })
+  })
+  it('should return all the records in the database', (done) => {
+    api.get('/social')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body).to.be.an('array')
+      done()
+    })
+  })
+})
+
 // GET projects/:id
 describe('GET /projects/:id', () => {
   // done is a function
@@ -53,3 +81,81 @@ describe('GET /projects/:id', () => {
     })
   })
 })
+
+// GET skills me
+describe('GET /skills', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.get('/skills')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+  it('should return an array', (done) => {
+    api.get('/skills')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body.length).to.equal(response.body.length)
+      done()
+    })
+  })
+  it('should return all the records in the database', (done) => {
+    api.get('/skills')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body).to.be.an('array')
+      done()
+    })
+  })
+})
+
+// GET education/:id
+describe('GET /education/:id', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.get('/education/:id')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+
+  it('should return an object containing the fields "title", "description" and "color"', (done) => {
+    api.get('/education/1')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body).to.have.property('school')
+      expect(response.body).to.have.property('subjects')
+      done()
+    })
+  })
+})
+
+// GET work me
+describe('GET /work', () => {
+  // done is a function
+  it('should return a 200 response', (done) => {
+    api.get('/work')
+    .set('Accept', 'application/json')
+    .expect(200, done)
+  })
+  it('should return an array', (done) => {
+    api.get('/work')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body.length).to.equal(response.body.length)
+      done()
+    })
+  })
+  it('should return all the records in the database', (done) => {
+    api.get('/work')
+    .set('Accept', 'application/json')
+    .end((error, response) => {
+      expect(error).to.be.a('null')
+      expect(response.body).to.be.an('array')
+      done()
+    })
+  })
+})
+
